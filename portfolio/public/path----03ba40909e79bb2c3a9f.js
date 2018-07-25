1b874a9c15d6de9efbdd3cfec1e5b8eed532424c
Brad Hankee <brad.hankee@gmail.com>
2018-07-25 16:56:42 -0400

    Added font awesome icons.

---
+++ portfolio/public/path----03ba40909e79bb2c3a9f.js
@@ -0,0 +1,2 @@
+webpackJsonp([60335399758886],{103:function(t,a){t.exports={data:{site:{siteMetadata:{title:"Brad Hankee Portfolio"}}},layoutContext:{}}}});
+//# sourceMappingURL=path----03ba40909e79bb2c3a9f.js.map
