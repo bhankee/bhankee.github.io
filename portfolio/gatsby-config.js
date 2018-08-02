547e9abf983f3510da69e76d4c8d48ce44b94851
Brad Hankee <brad.hankee@gmail.com>
2018-08-02 16:39:27 -0400

    Updates

--- portfolio/gatsby-config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  siteMetadata: {
-    title: 'Brad Hankee Portfolio',
-  },
-  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-sass'],
-}
--- portfolio/src/pages/resume.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import Link from 'gatsby-link'
-import Resume from '../components/Resume'
-
-const ResumePage = () => <Resume />
-
-export default ResumePage
